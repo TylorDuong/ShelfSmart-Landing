20bae872fba6f054bbfe43f06bb88a66bebf0d38
undTurt <88011225+undTurt@users.noreply.github.com>
2026-05-08 19:38:30 -0700

    about us

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Waitlist from "./pages/Waitlist";
 import Contact from "./pages/Contact";
+import About from "./pages/About";
 
 export default function App() {
   return (
@@ -10,6 +11,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/waitlist" element={<Waitlist />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
